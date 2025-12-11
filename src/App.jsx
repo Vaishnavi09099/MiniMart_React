7d9265d79cb820a77a0f6a14ac6d2c1a509e7beb
Vaishnavi <Vaishnavi09.099@gmail.com>
2025-12-11 12:32:35 +0530

    Added search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import AllCards from "./components/AllCards"
+import {data} from "./utils/data"
 
 
 
 function App() {
+  const [val,setVal] = useState('')
+  const [filterData,setFilterData] = useState(data)
+  function filterHandler(){
+    let res = data.filter((item)=> item.title.toLowerCase().includes(val.toLowerCase()))
+    setFilterData(res)
+  }
   
 
   return (
     <div>
-      <h1>GlamFinder - Find Glam that suits you!</h1>
+      <h1>Minimart - Find what you need!</h1>
       <div>
-        <input type="text" placeholder="Find your Glam......" />
-        <button>Search</button>
+        <input type="text" placeholder="what do you want???......" onChange={(e)=>setVal(e.target.value)}/>
+        <button onClick={filterHandler}>Search</button>
       </div>
-      <AllCards />
+      <AllCards data={filterData}/>
       
     </div>
   )
