af401adecd8eb7da70213f25d17452ca973bcd8f
Vaishnavi <Vaishnavi09.099@gmail.com>
2025-12-11 16:35:14 +0530

    Added styling using tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,16 @@ function App() {
   
 
   return (
-    <div>
-      <h1>Minimart - Find what you need!</h1>
+    <div className="bg-gray-300 text-center min-h-screen max-w-[100vw] flex flex-col items-center">
+      <h1 class="text-3xl pt-4  ">Minimart - Find what you need!</h1>
       <div>
-        <input type="text" placeholder="what do you want???......" onChange={(e)=>setVal(e.target.value)}/>
-        <button onClick={filterHandler}>Search</button>
+        <div className="mt-5">
+        <input className="p-3 rounded-tl-xl rounded-bl-xl md:pr-[50px] bg-white focus:outline-none" type="text" placeholder="what do you want???......" onChange={(e)=>setVal(e.target.value)}/>
+        <button className="p-3 px-6 rounded-tr-xl rounded-br-xl  md:pl-[40px] bg-green-400" onClick={filterHandler}>Search</button>
+        </div>
       </div>
-      <AllCards data={filterData}/>
+      
+      {filterData.length <= 0 ? <h1 className="text-4xl mt-10">No item found</h1> : <AllCards data={filterData} />}
       
     </div>
   )
--- src/components/AllCards.jsx
+++ src/components/AllCards.jsx
@@ -3,14 +3,20 @@
 function AllCards({data}){
     
     return (
-        <div style={{display:"flex",flexWrap:"wrap",gap:"30px"}}>
+        <div className="mt-10 flex flex-wrap justify-around w-[80%] gap-10 mb-10">
 
             {data.map(singleItem => (
-                 <div key={singleItem.id} style={{backgroundColor:"green",maxWidth:"350px",height:"400px",padding:"20px",overflow:"hidden"}}>
-                <img src={singleItem.thumbnail} style={{width:"200px" ,height:"200px"}} alt="" />
-                <h2>{singleItem.title}</h2>
-                <p>${singleItem.price}</p>
-                <p style={{opacity:"0.5"}}>{singleItem.description}</p>
+                 <div
+                  key={singleItem.id}
+                  className="bg-red-300 w-[25%] h-[500px] p-4 rounded-3xl shadow-xl"
+                  
+                  >
+                <img src={singleItem.thumbnail} className="aspect-square-w-[100%] object-cover h-[60%] rounded-2xl bg-[#E9EEEE] shadow-xl" alt="" />
+                <div className="flex items-center justify-between">
+                <h2 className="text-left my-2 text-xl line-clamp-1">{singleItem.title}</h2>
+                <p className="text-3xl">${singleItem.price}</p>
+                </div>
+                <p className="text-left line-clamp-1 lg:line-clamp-3 ">{singleItem.description}</p>
             </div>
            
             ))}
